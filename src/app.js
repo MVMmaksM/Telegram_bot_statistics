2e793f0296ed882032442f8a7bf0b0b85d22b570
MVMmaksM <vaganov.maksim94@yandex.ru>
2024-07-21 15:35:53 +0500

    create auth

--- src/app.js
+++ src/app.js
@@ -3,17 +3,24 @@ const getStatistics = require('./service/statistics/statistics.js');
 const isAuth = require('./service/authentication/auth.js');
 const Telegraf = require('telegraf').Telegraf;
 const bot = new Telegraf(config.bot_api_token);
+const createInstance = require('./db/createInstance.js');
+
+global.instance = createInstance();
 
 bot.start((context)=> {
-    context.reply("Введите пароль:");
+    context.reply(`Введите логин и пароль в формате "логин пароль":`);
 })
 
 bot.use(async(context, next)=>{
-    if(isAuth(context.message.text)){
-        next();
-        return;
-    }
-    await context.telegram.sendMessage(context.message.chat.id, "Неверный пароль");
+    const credentials = context.message.text.split(" ");
+
+    if(credentials.length === 2){
+        if(isAuth(credentials[0], credentials[1])){
+            next();
+            return;
+        }
+    }    
+    await context.telegram.sendMessage(context.message.chat.id, "Неверный логин или пароль");
 });
 
 bot.use(async(context, _)=>{
@@ -31,3 +38,4 @@ bot.launch();
 
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
+
--- src/service/authentication/auth.js
+++ src/service/authentication/auth.js
@@ -1,7 +1,15 @@
 const config = require('../../config/app.conf');
+const md5 = require('js-md5');
 
-function isAuth(password){
-    if(password === config.password)
+async function isAuth(login, password){
+    const instance  = global.instance;
+
+    const user = await instance
+        .select("id")
+        .from("users")
+        .where({username: login, password: md5(password)});
+
+    if(user)
         return true;
     return false;
 }
